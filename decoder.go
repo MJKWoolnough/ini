993a39dde99bca937f8095a110c6914dd778e5dd
Michael Woolnough <michael.woolnough@gmail.com>
2016-07-07 13:52:40 +0100

    moved common code to decode func

--- decoder.go
+++ decoder.go
@@ -20,46 +20,33 @@ type decoder struct {
 type Option func(*decoder)
 
 func Decode(r io.Reader, v interface{}, options ...Option) error {
-	d := decoder{
-		Tokeniser:       parser.NewReaderTokeniser(r),
-		NameValueDelim:  '=',
-		SubSectionDelim: '/',
-	}
-	for _, o := range options {
-		o(&d)
-	}
-	return d.decode(v)
+	return decode(parser.NewReaderTokeniser(r), v, options...)
 }
 
 func DecodeString(s string, v interface{}, options ...Option) error {
-	d := decoder{
-		Tokeniser:       parser.NewStringTokeniser(s),
-		NameValueDelim:  '=',
-		SubSectionDelim: '/',
-	}
-	for _, o := range options {
-		o(&d)
-	}
-	return d.decode(v)
+	return decode(parser.NewStringTokeniser(s), v, options...)
 }
 
 func DecoderBytes(b []byte, v interface{}, options ...Option) error {
+	return decode(parser.NewByteTokeniser(b), v, options...)
+}
+
+func decode(t parser.Tokeniser, v interface{}, options ...Option) error {
 	d := decoder{
-		Tokeniser:       parser.NewByteTokeniser(b),
+		Tokeniser:       t,
 		NameValueDelim:  '=',
 		SubSectionDelim: '/',
 	}
 	for _, o := range options {
 		o(&d)
 	}
-	return d.decode(v)
-}
-
-func (d *decoder) decode(v interface{}) error {
 	d.TokeniserState(d.name)
 	rv := reflect.ValueOf(v)
 
 	if k := rv.Kind(); k != reflect.Map {
+		if rv.Type().Key().Kind() != reflect.String {
+			return ErrInvalidKey
+		}
 		switch rv.Type().Elem().Kind() {
 		case reflect.Map:
 		case reflect.String:
@@ -94,4 +81,5 @@ var (
 	ErrNotPointer       = errors.New("need pointer to type")
 	ErrInvalidType      = errors.New("needs map, slice or struct type")
 	ErrInvalidSliceType = errors.New("need slice of structs")
+	ErrInvalidKey       = errors.New("maps require string keys")
 )
