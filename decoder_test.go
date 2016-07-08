e60eae41fd91abf26e8cae86a6a88d9bb228d9bc
Michael Woolnough <michael.woolnough@gmail.com>
2016-07-08 17:19:51 +0100

    added tests for map[string]struct

--- decoder_test.go
+++ decoder_test.go
@@ -1,6 +1,7 @@
 package ini_test
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/MJKWoolnough/ini"
@@ -296,6 +297,33 @@ func TestMapMapStringCustomTypes(t *testing.T) {
 	} else {
 		if b["E"] != "F" {
 			t.Errorf("Test: expecting \"SectionB/E\" == \"F\", got %q", b["E"])
+		}
+	}
+}
+
+func TestMapStruct(t *testing.T) {
+	type Test1 struct {
+		A string
+		C uint8
+	}
+	tests := []struct {
+		Input  string
+		Output interface{}
+		Result interface{}
+	}{
+		{"A=B\nC=4", map[string]Test1{"": {"B", 4}}, map[string]Test1{}},
+		{"[Section1]\nA=FG\nC=123", map[string]Test1{"": {"", 0}, "Section1": {"FG", 123}}, map[string]Test1{}},
+		{"A=B\nC=4\n[Section1]\nA=FG\nC=123", map[string]Test1{"": {"B", 4}, "Section1": {"FG", 123}}, map[string]Test1{}},
+	}
+
+	for n, test := range tests {
+		err := ini.DecodeString(test.Input, test.Result)
+		if err != nil {
+			t.Errorf("Test %d: unexpected error, %s", n+1, err)
+			continue
+		}
+		if !reflect.DeepEqual(test.Result, test.Output) {
+			t.Errorf("Test %d: result does not match expected", n+1)
 		}
 	}
 }
